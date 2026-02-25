bc60414e35b4ac747274fb0281333edd60fbd24a
edsmith3ncat <edsmith3@aggies.ncat.edu>
2026-02-25 11:10:48 -0500

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+backupserverip="192.168.56.20"
+backupusername="backupuser"
+backuproot="/backups/server1"
+logfolder="/data/
+
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+finaldest="$backuproot/$timestamp"
+logfile="$logfolder/backup_$timestamp.log"
+
+mkdir -p "$logfolder"
+exec > >(tee -a "$logfile") 2>&1
+
+ssh -o ConnectTimeout=5 "$backupusername@$backupserverip" "exit"
+if [ $? -ne 0 ]; then
+    echo "Server connection failed"
+    exit 1
+fi
+
+ssh "$backupusername@$backupserverip" "mkdir -p $finaldest"
+if [ $? -ne 0 ]; then
+    echo "Cannot create folder"
+    exit 1
+fi
+
+rsync -avz --exclude='lost+found' /data/ "$backupusername@$backupserverip:$finaldest/data_file"
+if [ $? -ne 0 ]; then
+    echo "Cannot copy data"
+    exit 1
+fi
+
+ssh "$backupusername@$backupserverip" "mkdir -p $finaldest/configs"
+rsync -avz /etc/fstab /etc/ssh/sshd_config "$backupusername@$backupserverip:$finaldest/configs"
+if [ $? -ne 0 ]; then
+    echo "Cannot copy config"
+    exit 1
+fi
+exit 0
